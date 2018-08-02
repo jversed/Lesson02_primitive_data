b0d089282b11099099daa2b3e1d572c13466cbec
Alexandr Slobodnichenko <aslobodnichenko@gmail.com>
2018-08-02 04:29:16 +0300

    language changed

--- src/Begin.java
+++ src/Begin.java
@@ -5,14 +5,15 @@ public class Begin {
 	public static void main(String[] args) {
 		
 		Scanner reader = new Scanner(System.in);  // Reading from System.in
-		System.out.println("Vvedite storony kvadrata: ");
+		System.out.println("Enter side of the square: ");
 		double a = reader.nextDouble(); // Scans the next token of the input as an double.
 		reader.close(); // Finished with the scanner to avoid resource leaks.
 		double p = a*4;
 		double s = Math.pow(a,2);
-		System.out.println("Perimtr="+p);
-		System.out.println("Ploshad="+s);
-	// Tasks begin1-2.
+		
+		System.out.println("Perimeter="+p);
+		System.out.println("Area="+s);
+		// Tasks begin1-2.
 	}
 
 }
